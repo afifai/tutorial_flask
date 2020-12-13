a9374d4337ae9e680ffb391be912e3779d1f66ca
Afif Akbar Iskandar <afifai@sci.ui.ac.id>
2020-12-13 23:19:47 +0700

    episode_2

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/halo')
-def hello_world():
-    return '<h5>Hello, NgodingPython!</h5>'
+konten = [
+    {
+        'penulis': 'Afif A. Iskandar',
+        'judul': 'Postingan Pertama',
+        'sinopsis': 'Ini adalah postingan pertama',
+        'isi': 'Ini adalah isi dari postingan pertama',
+        'tanggal': '12 Desember 2020',
+        'jam': '16.00'
+    },
+    {
+        'penulis': 'NgodingPython',
+        'judul': 'Tutorial Flask',
+        'sinopsis': 'Ini adalah tutorial flask',
+        'isi': 'Ini adalah isi dari tutorial flask',
+        'tanggal': '13 Desember 2020',
+        'jam': '18.00'
+    }
+]
 
 @app.route('/')
 def home():
-    return '<p>Nama Saya Afif</p>'
+    return render_template('home.html', konten=konten, judul='Beranda')
+
+@app.route('/tentang/')
+def tentang():
+    return render_template('tentang.html', judul='Tentang')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=1234)
+    app.run(debug=True)
--- readme.md
+++ readme.md
@@ -1,8 +1,8 @@
 # Tutorial Flask
 
-## Branch : episode_1
+## Branch : episode_2
 
-Ini adalah branch / cabang untuk code yang ditulis pada `episode 1` dari video [Tutorial Flask](https://www.youtube.com/watch?v=oAAYg55nfZI&list=PLl-Zj2iuqlwvDv3dbTenevb5ZFW5ne9u2) dari [NgodingPython](https://youtube.com/NgodingPython).
+Ini adalah branch / cabang untuk code yang ditulis pada `episode 2` dari video [Tutorial Flask](https://www.youtube.com/watch?v=oAAYg55nfZI&list=PLl-Zj2iuqlwvDv3dbTenevb5ZFW5ne9u2) dari [NgodingPython](https://youtube.com/NgodingPython).
 
 Untuk video per-episode, dapat dilihat pada branch yang bersesuaian, dan untuk kode terkini, silahkan lihat branch `main`.
 
